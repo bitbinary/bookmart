e2bae2ec8e3a4c25a703005bf880b2035ca90724
ameljohny <ameljohny.mail@gmail.com>
2021-10-02 13:03:18 +1000

    updated loading page

--- src/utils/shared/PageLoading.js
+++ src/utils/shared/PageLoading.js
@@ -1,9 +1,23 @@
+import { LinearProgress } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
 
 export default function PageLoading() {
   return (
-    <div>
-      <p>Loading...</p>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        backgroundColor: '#fdfdfd',
+        flexGrow: 1,
+        flexShrink: 0,
+        flexFlow: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+      }}
+    >
+      Loading Page
+      <LinearProgress sx={{ width: '300px' }} />
+    </Box>
   );
 }

50fe838783499e1ca8843641cd8fea7b6f6d7b12
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2021-10-04 21:12:57 +1100

    additional changes

--- src/App.js
+++ src/App.js
@@ -1,71 +1,80 @@
 import './App.scss';
 import './configs/theme.js';
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
+   BrowserRouter as Router,
+   Route,
+   Switch,
+   Redirect,
 } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import ResetPassword from './pages/ResetPassword/ResetPassword';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Landing from './pages/Landing/Landing';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './configs/firebase';
 import PageLoading from './utils/shared/PageLoading';
 import Navbar from './utils/shared/Navbar';
+import AuthenticatedLanding from './pages/Landing/AuthenticatedLanding';
+import Tables from './AdminPages/Tables';
 function App() {
-  return (
-    <div className="app">
-      <Navbar />
-      <Router>
-        <Switch>
-          <Route exact path="/app" component={Protected} />
-          <Route path="/" component={Public} />
-        </Switch>
-      </Router>
-    </div>
-  );
+   return (
+      <div className='app'>
+         <Navbar />
+         <Router>
+            <Switch>
+               <Route exact path='/logout' component={Protected} />
+
+               <Route exact path='/app' component={Protected} />
+               <Route path='/' component={Public} />
+            </Switch>
+         </Router>
+      </div>
+   );
 }
 
 const Protected = () => {
-  const [user, loading] = useAuthState(auth);
-  if (loading) return <PageLoading />;
-  if (!user) return <Redirect to="/" from="/app" />;
-  const { emailVerified } = user;
-  return (
-    <Route
-      render={() => {
-        return <PrivateRoutes emailVerified={emailVerified} />;
-      }}
-    />
-  );
+   const [user, loading] = useAuthState(auth);
+   if (loading) return <PageLoading />;
+   if (!user) return <Redirect to='/' from='/app' />;
+   const { emailVerified } = user;
+   return (
+      <Route
+         render={() => {
+            return <PrivateRoutes emailVerified={emailVerified} />;
+         }}
+      />
+   );
 };
 const Public = () => {
-  const [user, loading] = useAuthState(auth);
-  return (
-    <Route
-      render={() => {
-        if (user && !loading) return <Protected />;
-        return <PublicRoutes />;
-      }}
-    />
-  );
+   const [user, loading] = useAuthState(auth);
+   return (
+      <Route
+         render={() => {
+            if (user && !loading) return <Protected />;
+            return <PublicRoutes />;
+         }}
+      />
+   );
 };
 
 const PrivateRoutes = ({ emailVerified }) => {
-  // if (!emailVerified) return <ConfirmEmail />;
-  return (
-    <Switch>
-      <Route path="/" component={Dashboard} />
-    </Switch>
-  );
+   // if (!emailVerified) return <ConfirmEmail />;
+   return (
+      <Switch>
+         <Route path='/logout' component={Dashboard} />
+         <Route path='/tables' component={Tables} />
+
+         <Route path='/' component={AuthenticatedLanding} />
+      </Switch>
+   );
 };
 const PublicRoutes = () => {
-  return (
-    <Switch>
-      <Route exact path="/reset" component={ResetPassword} />
-      <Route path="/" component={Login} />
-    </Switch>
-  );
+   return (
+      <Switch>
+         <Route exact path='/reset' component={ResetPassword} />
+         <Route path='/login' component={Login} />
+         <Route path='/' component={Landing} />
+      </Switch>
+   );
 };
 export default App;

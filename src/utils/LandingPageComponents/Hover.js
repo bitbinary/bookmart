a69814d7782c3d06cca5c5a52669435564586d73
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2021-10-04 21:10:25 +1100

    Added home page sections for logged in userr and not logged in user

---
+++ src/utils/LandingPageComponents/Hover.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Popover from '@mui/material/Popover';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
+import Filters from './Filters';
+import { FilterList } from '@material-ui/icons';
+
+export default function Hover() {
+   return (
+      <PopupState variant='popover' popupId='demo-popup-popover'>
+         {(popupState) => (
+            <div>
+               <FilterList
+                  sx={{
+                     fontFamily: 'Poynter,Georgia,serif',
+                     fontStyle: 'italic',
+                     backgroundColor: 'yellow',
+                  }}
+                  {...bindTrigger(popupState)}
+               />
+               <Popover
+                  {...bindPopover(popupState)}
+                  anchorOrigin={{
+                     vertical: 'bottom',
+                     horizontal: 'center',
+                  }}
+                  transformOrigin={{
+                     vertical: 'top',
+                     horizontal: 'center',
+                  }}
+               >
+                  <Filters />
+               </Popover>
+            </div>
+         )}
+      </PopupState>
+   );
+}

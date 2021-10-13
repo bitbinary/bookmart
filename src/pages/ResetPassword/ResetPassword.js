42669aa4b00c625b5f3f86c4d161feac3f0b2735
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2021-10-14 00:45:48 +1100

    error handling implemented for user authentication

--- src/pages/ResetPassword/ResetPassword.js
+++ src/pages/ResetPassword/ResetPassword.js
@@ -5,34 +5,49 @@ import { Link } from 'react-router-dom';
 import { auth, sendPasswordResetEmail } from '../../configs/firebase';
 import './resetPassword.css';
 function ResetPassword() {
-  const [email, setEmail] = useState('');
-  const [user, loading] = useAuthState(auth);
-  const history = useHistory();
-  useEffect(() => {
-    if (loading) return;
-    if (user) history.replace('/');
-  }, [user, loading, history]);
-  return (
-    <div className="reset">
-      <div className="reset__container">
-        <input
-          type="text"
-          className="reset__textBox"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
-        />
-        <button
-          className="reset__btn"
-          onClick={() => sendPasswordResetEmail(email)}
-        >
-          Send password reset email
-        </button>
-        <div>
-          Don't have an account? <Link to="/login">Register</Link> now.
-        </div>
+   const [email, setEmail] = useState('');
+   const [showAlert, setShowAlert] = useState(false);
+
+   const [user, loading] = useAuthState(auth);
+   const history = useHistory();
+
+   const sendEmail = (email) => {
+      sendPasswordResetEmail(email);
+      setShowAlert(true);
+   };
+
+   useEffect(() => {
+      if (loading) return;
+      if (user) history.replace('/');
+   }, [user, loading, history]);
+   return (
+      <div className='reset'>
+         <div className='reset__container'>
+            <input
+               type='text'
+               className='reset__textBox'
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
+               placeholder='E-mail Address'
+            />
+            <button className='reset__btn' onClick={() => sendEmail(email)}>
+               Send password reset email
+            </button>
+            <div>
+               Don't have an account? <Link to='/login'>Register</Link> now.
+            </div>
+            {showAlert && (
+               <>
+                  <div style={{ color: 'blue' }}>
+                     *Link sent to the given e-mail.
+                  </div>
+                  <div style={{ color: 'red' }}>
+                     *Verify your e-mail id, if not found.
+                  </div>
+               </>
+            )}
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 export default ResetPassword;
